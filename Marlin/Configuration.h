dd101334c6c4c2cd16041d707fd8bcb56f75062e
Gregory Holloway <info@thelittlebox.co>
2016-03-01 12:23:12 +0000

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -410,7 +410,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 #define Z_MIN_POS 0
 #define X_MAX_POS 260
 #define Y_MAX_POS 240
-#define Z_MAX_POS 280
+#define Z_MAX_POS 300
 
 //===========================================================================
 //========================= Filament Runout Sensor ==========================
